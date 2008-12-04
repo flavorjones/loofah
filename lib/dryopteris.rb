ef1a6cb87c9e3407ba9707bd7453d65ea4a05d65
Mike Dalessio <mike@csa.net>
2008-12-04 13:26:38 -0500

    API and basic sax parser.

--- lib/dryopteris.rb
+++ lib/dryopteris.rb
@@ -1,14 +1,23 @@
 
 require 'rubygems'
-gem 'nokogiri', '>=1.05'
+gem 'nokogiri', '>=1.0.5'
 require 'nokogiri'
 
 require File.join(File.dirname(__FILE__), 'whitelist')
 
 module Dryopteris
 
-  def self.sanitize(string_or_io)
-    doc = Nokogiri(string_or_io)
+  def self.sanitize(string_or_io, encoding=nil)
+    parser = Nokogiri::HTML::SAX::Parser.new(Dryopteris::Document.new)
+    args = [string_or_io]
+    args << encoding unless encoding.nil?
+    parser.parse_memory(*args)
+  end
+
+  class Document < Nokogiri::XML::SAX::Document
+    def start_element name, attrs = []
+      puts "found element: #{name}"
+    end
   end
 
 end
