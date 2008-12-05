2783dc7950916ec43e6bdb6524fd38c0486ec258
Bryan Helmkamp <bryan@brynary.com>
2008-12-04 19:09:56 -0500

    Adding Dryopteris::VERSION

--- lib/dryopteris.rb
+++ lib/dryopteris.rb
@@ -1,3 +1,7 @@
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__))) unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
 
 require "dryopteris/sanitize"
+
+module Dryopteris
+  VERSION = '0.1'
+end
