b0b4504f39cc43aba4c7136931d7d8baa0fefd53
Mike Dalessio <mike@csa.net>
2009-05-29 18:43:14 -0400

    initial API tests. go red? check.

---
+++ test/test_api.rb
@@ -0,0 +1,64 @@
+require File.expand_path(File.join(File.dirname(__FILE__), 'helper'))
+
+class TestApi < Test::Unit::TestCase
+
+  def setup
+    @tags = "<div>a</div><div>b</div>"
+  end
+
+  def test_dryopteris_method
+    # Dryopteris(@tags)
+    flunk "not implemented"
+  end
+
+  def test_dryopteris_html_method
+    # Dryopteris::HTML(@tags)
+    flunk "not implemented"
+  end
+
+  def test_dryopteris_xml_method
+    # Dryopteris::XML(@tags)
+    flunk "not implemented"
+  end
+
+  def test_dryopteris_html_document_method
+    # Dryopteris::HTML::Document(@tags)
+    flunk "not implemented"
+  end
+
+  def test_dryopteris_html_document_parse_method
+    # Dryopteris::HTML::Document.parse(@tags)
+    flunk "not implemented"
+  end
+
+  def test_dryopteris_html_document_fragment_method
+    # Dryopteris::HTML::DocumentFragment(@tags)
+    flunk "not implemented"
+  end
+
+  def test_dryopteris_html_document_fragment_parse_method
+    # Dryopteris::HTML::DocumentFragment.parse(@tags)
+    flunk "not implemented"
+  end
+
+  def test_dryopteris_xml_document_method
+    # Dryopteris::XML::Document(@tags)
+    flunk "not implemented"
+  end
+
+  def test_dryopteris_xml_document_parse_method
+    # Dryopteris::XML::Document.parse(@tags)
+    flunk "not implemented"
+  end
+
+  def test_dryopteris_xml_document_fragment_method
+    # Dryopteris::XML::DocumentFragment(@tags)
+    flunk "not implemented"
+  end
+
+  def test_dryopteris_xml_document_fragment_parse_method
+    # Dryopteris::XML::DocumentFragment.parse(@tags)
+    flunk "not implemented"
+  end
+
+end
