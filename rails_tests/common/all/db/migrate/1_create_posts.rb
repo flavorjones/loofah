d7376fac432aac34c7287e6fe2543d0bc0bc7922
Mike Dalessio <mike@csa.net>
2009-09-18 15:24:49 -0400

    script to create projects for specific rails versions, along with some files to be deposited in each.

---
+++ rails_tests/common/all/db/migrate/1_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration
+  def self.up
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+    end
+  end
+  
+  def self.down
+  end
+end
