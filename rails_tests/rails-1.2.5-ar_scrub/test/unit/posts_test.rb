c0d33564f965ec7184c46c2652649df9fc3c7ebd
Mike Dalessio <mike@csa.net>
2009-09-19 01:13:58 -0400

    added tests for rails 2.2, 2.1, 2.0 and 1.2.

---
+++ rails_tests/rails-1.2.5-ar_scrub/test/unit/posts_test.rb
@@ -0,0 +1,10 @@
+require File.join(File.dirname(__FILE__), "../test_helper")
+
+class PostsTest < Test::Unit::TestCase
+  def test_loofah_scrubbing
+    post = Post.new :title => "<script>yo dawg</script>", :body => "<script>omgwtfbbq</script>"
+    post.save!
+    assert_equal "<script>yo dawg</script>", post.title
+    assert_equal "omgwtfbbq", post.body
+  end
+end
