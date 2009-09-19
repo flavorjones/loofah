3806cc76597e0f5cdef543e3dde3b71f1eb24230
Mike Dalessio <mike@csa.net>
2009-09-19 00:42:26 -0400

    rails test app calls config.gem for loofah

--- rails_tests/rails-2.3.4-ar_scrub/config/environment.rb
+++ rails_tests/rails-2.3.4-ar_scrub/config/environment.rb
@@ -19,6 +19,7 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "loofah"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
