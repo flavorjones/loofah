30969989af8ba4ceac83557dcaa3c429772dcc1b
Mike Dalessio <mike@csa.net>
2009-09-21 18:53:43 -0400

    added xss_foliate implementation and tests.

--- lib/loofah.rb
+++ lib/loofah.rb
@@ -199,7 +199,9 @@ end
 if defined? Rails.configuration # rails 2.1 and later
   Rails.configuration.after_initialize do
     require 'loofah/active_record'
+    require 'loofah/xss_foliate'
   end
 elsif defined? ActiveRecord::Base # rails 2.0
   require 'loofah/active_record'
+  require 'loofah/xss_foliate'
 end
--- test/test_active_record.rb
+++ test/test_active_record.rb
@@ -16,15 +16,21 @@ class TestActiveRecord < Test::Unit::TestCase
       end
     end
 
-    context "scrubbing field as a fragment" do
+    context "scrubbing a single field as a fragment" do
       setup do
         Post.html_fragment :html_string, :scrub => :prune
+        assert ! Post.xss_foliated?
         @post = Post.new :html_string => HTML_STRING, :plain_text => PLAIN_TEXT
       end
 
       should "scrub the specified field" do
         Loofah.expects(:scrub_fragment).with(HTML_STRING, :prune).once
         Loofah.expects(:scrub_fragment).with(PLAIN_TEXT, :prune).never
+        @post.valid?
+      end
+
+      should "only call scrub_fragment once" do
+        Loofah.expects(:scrub_fragment).once
         @post.valid?
       end
 
@@ -35,7 +41,7 @@ class TestActiveRecord < Test::Unit::TestCase
       end
     end
 
-    context "scrubbing field as a document" do
+    context "scrubbing a single field as a document" do
       setup do
         Post.html_document :html_string, :scrub => :strip
         @post = Post.new :html_string => HTML_STRING, :plain_text => PLAIN_TEXT
@@ -44,6 +50,11 @@ class TestActiveRecord < Test::Unit::TestCase
       should "scrub the specified field, but not other fields" do
         Loofah.expects(:scrub_document).with(HTML_STRING, :strip).once
         Loofah.expects(:scrub_document).with(PLAIN_TEXT, :strip).never
+        @post.valid?
+      end
+
+      should "only call scrub_document once" do
+        Loofah.expects(:scrub_document).once
         @post.valid?
       end
 
