7609062fa95ae510e788f80c3286e9f80b4272bb
Mike Dalessio <mike@csa.net>
2009-09-22 17:32:45 -0400

    configuring rails 1.2.5 test to run xss_foliate

--- rails_test/rails-1.2.5-xss_foliate/config/environment.rb
+++ rails_test/rails-1.2.5-xss_foliate/config/environment.rb
@@ -10,6 +10,9 @@ RAILS_GEM_VERSION = '1.2.5' unless defined? RAILS_GEM_VERSION
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+LOOFAH_XSS_FOLIATE_ALL_MODELS = true
+gem 'loofah'
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here
 
@@ -58,3 +61,4 @@ end
 # Mime::Type.register "application/x-mobile", :mobile
 
 # Include your application configuration below
+require 'loofah'
