98022432f8ac1e0e96a276381770b9bab24f3ea5
Mike Dalessio <mike@csa.net>
2009-09-25 18:41:33 -0400

    added coverage to active_record to make sure we can accept string attribute names

--- test/test_active_record.rb
+++ test/test_active_record.rb
@@ -17,50 +17,81 @@ class TestActiveRecord < Test::Unit::TestCase
     end
 
     context "scrubbing a single field as a fragment" do
-      setup do
-        Post.html_fragment :html_string, :scrub => :prune
-        assert ! Post.xss_foliated?
-        @post = Post.new :html_string => HTML_STRING, :plain_text => PLAIN_TEXT
+      context "using a symbol to indicate the attribute" do
+        setup do
+          Post.html_fragment :html_string, :scrub => :prune
+          assert ! Post.xss_foliated?
+          @post = Post.new :html_string => HTML_STRING, :plain_text => PLAIN_TEXT
+        end
+
+        should "scrub the specified field" do
+          Loofah.expects(:scrub_fragment).with(HTML_STRING, :prune).once
+          Loofah.expects(:scrub_fragment).with(PLAIN_TEXT, :prune).never
+          @post.valid?
+        end
+
+        should "only call scrub_fragment once" do
+          Loofah.expects(:scrub_fragment).once
+          @post.valid?
+        end
+
+        should "generate strings" do
+          @post.valid?
+          assert_equal String, @post.html_string.class
+          assert_equal HTML_STRING, @post.html_string
+        end
       end
 
-      should "scrub the specified field" do
-        Loofah.expects(:scrub_fragment).with(HTML_STRING, :prune).once
-        Loofah.expects(:scrub_fragment).with(PLAIN_TEXT, :prune).never
-        @post.valid?
-      end
+      context "using a string to indicate the attribute" do
+        setup do
+          Post.html_fragment 'html_string', :scrub => :prune
+          assert ! Post.xss_foliated?
+          @post = Post.new :html_string => HTML_STRING, :plain_text => PLAIN_TEXT
+        end
 
-      should "only call scrub_fragment once" do
-        Loofah.expects(:scrub_fragment).once
-        @post.valid?
-      end
-
-      should "generate strings" do
-        @post.valid?
-        assert_equal String, @post.html_string.class
-        assert_equal HTML_STRING, @post.html_string
+        should "scrub the specified field" do
+          Loofah.expects(:scrub_fragment).with(HTML_STRING, :prune).once
+          Loofah.expects(:scrub_fragment).with(PLAIN_TEXT, :prune).never
+          @post.valid?
+        end
       end
     end
 
     context "scrubbing a single field as a document" do
-      setup do
-        Post.html_document :html_string, :scrub => :strip
-        @post = Post.new :html_string => HTML_STRING, :plain_text => PLAIN_TEXT
+      context "using a symbol to indicate the attribute" do
+        setup do
+          Post.html_document :html_string, :scrub => :strip
+          @post = Post.new :html_string => HTML_STRING, :plain_text => PLAIN_TEXT
+        end
+
+        should "scrub the specified field, but not other fields" do
+          Loofah.expects(:scrub_document).with(HTML_STRING, :strip).once
+          Loofah.expects(:scrub_document).with(PLAIN_TEXT, :strip).never
+          @post.valid?
+        end
+
+        should "only call scrub_document once" do
+          Loofah.expects(:scrub_document).once
+          @post.valid?
+        end
+
+        should "generate strings" do
+          @post.valid?
+          assert_equal String, @post.html_string.class
+        end
       end
 
-      should "scrub the specified field, but not other fields" do
-        Loofah.expects(:scrub_document).with(HTML_STRING, :strip).once
-        Loofah.expects(:scrub_document).with(PLAIN_TEXT, :strip).never
-        @post.valid?
-      end
+      context "using a string to indicate the attribute" do
+        setup do
+          Post.html_document 'html_string', :scrub => :strip
+          @post = Post.new :html_string => HTML_STRING, :plain_text => PLAIN_TEXT
+        end
 
-      should "only call scrub_document once" do
-        Loofah.expects(:scrub_document).once
-        @post.valid?
-      end
-
-      should "generate strings" do
-        @post.valid?
-        assert_equal String, @post.html_string.class
+        should "scrub the specified field, but not other fields" do
+          Loofah.expects(:scrub_document).with(HTML_STRING, :strip).once
+          Loofah.expects(:scrub_document).with(PLAIN_TEXT, :strip).never
+          @post.valid?
+        end
       end
     end
 
