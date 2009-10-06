336d53d10716e76186b19e31891f1c7011dd711b
Mike Dalessio <mike@csa.net>
2009-10-06 02:49:52 -0400

    removing 'require "rubygems"' from loofah code

--- lib/loofah.rb
+++ lib/loofah.rb
@@ -1,6 +1,5 @@
 $LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__))) unless $LOAD_PATH.include?(File.expand_path(File.dirname(__FILE__)))
 
-require 'rubygems'
 require 'nokogiri'
 
 require 'loofah/html5/whitelist'
