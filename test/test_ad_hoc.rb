f46628e9e0835ea2ab56c58e2ee65cd0e281dae2
Mike Dalessio <mike@csa.net>
2009-11-21 17:25:07 -0500

    added an xml fragment scrub test

--- test/test_ad_hoc.rb
+++ test/test_ad_hoc.rb
@@ -33,6 +33,23 @@ class TestAdHoc < Test::Unit::TestCase
   end
 
   def test_xml_fragment_scrub
+    xml = Loofah.xml_fragment <<-EOXML
+      <employee deceased='true'>Abraham Lincoln</employee>
+      <employee deceased='false'>Abe Vigoda</employee>
+    EOXML
+    bring_out_your_dead = Loofah::Scrubber.new do |node|
+      if node.name == "employee" and node["deceased"] == "true"
+        node.remove
+        Loofah::Scrubber::STOP # don't bother with the rest of the subtree
+      end
+    end
+    assert_equal 2, xml.css("employee").length
+    
+    xml.scrub!(bring_out_your_dead)
+
+    employees = xml.css "employee"
+    assert_equal 1, employees.length
+    assert_equal "Abe Vigoda", employees.first.inner_text
   end
 
   def test_removal_of_illegal_tag
