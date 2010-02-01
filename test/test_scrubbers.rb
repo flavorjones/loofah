c1e87de039c7926d0a82ff63be39dda4a0605cb5
Mike Dalessio <mike@csa.net>
2010-02-01 18:46:04 -0500

    refactored tests

--- test/test_scrubbers.rb
+++ test/test_scrubbers.rb
@@ -3,9 +3,13 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'helper'))
 class TestScrubber < Test::Unit::TestCase
 
   [ Loofah::HTML::Document, Loofah::HTML::DocumentFragment ].each do |klass|
-    define_method "test_#{klass}_bad_sanitize_method" do
-      doc = klass.parse "<p>foo</p>"
-      assert_raises(Loofah::ScrubberNotFound) { doc.scrub! :frippery }
+    context klass do
+      context "bad scrub method" do
+        should "raise a ScrubberNotFound exception" do
+          doc = klass.parse "<p>foo</p>"
+          assert_raises(Loofah::ScrubberNotFound) { doc.scrub! :frippery }
+        end
+      end
     end
   end
 
@@ -20,125 +24,251 @@ class TestScrubber < Test::Unit::TestCase
   NOFOLLOW_FRAGMENT = '<a href="http://www.example.com/">Click here</a>'
   NOFOLLOW_RESULT   = '<a href="http://www.example.com/" rel="nofollow">Click here</a>'
 
-  def test_document_escape_bad_tags
-    doc = Loofah::HTML::Document.parse "<html><body>#{INVALID_FRAGMENT}</body></html>"
-    result = doc.scrub! :escape
+  context "Document" do
+    context "#scrub!" do
+      context ":escape" do
+        should "escape bad tags" do
+          doc = Loofah::HTML::Document.parse "<html><body>#{INVALID_FRAGMENT}</body></html>"
+          result = doc.scrub! :escape
 
-    assert_equal INVALID_ESCAPED, doc.xpath('/html/body').inner_html
-    assert_equal doc, result
+          assert_equal INVALID_ESCAPED, doc.xpath('/html/body').inner_html
+          assert_equal doc, result
+        end
+      end
+
+      context ":prune" do
+        should "prune bad tags" do
+          doc = Loofah::HTML::Document.parse "<html><body>#{INVALID_FRAGMENT}</body></html>"
+          result = doc.scrub! :prune
+
+          assert_equal INVALID_PRUNED, doc.xpath('/html/body').inner_html
+          assert_equal doc, result
+        end
+      end
+
+      context ":strip" do
+        should "strip bad tags" do
+          doc = Loofah::HTML::Document.parse "<html><body>#{INVALID_FRAGMENT}</body></html>"
+          result = doc.scrub! :strip
+
+          assert_equal INVALID_STRIPPED, doc.xpath('/html/body').inner_html
+          assert_equal doc, result
+        end
+      end
+
+      context ":whitewash" do
+        should "whitewash the markup" do
+          doc = Loofah::HTML::Document.parse "<html><body>#{WHITEWASH_FRAGMENT}</body></html>"
+          result = doc.scrub! :whitewash
+
+          assert_equal WHITEWASH_RESULT, doc.xpath('/html/body').inner_html
+          assert_equal doc, result
+        end
+      end
+
+      context ":nofollow" do
+        should "add a 'nofollow' attribute to hyperlinks" do
+          doc = Loofah::HTML::Document.parse "<html><body>#{NOFOLLOW_FRAGMENT}</body></html>"
+          result = doc.scrub! :nofollow
+
+          assert_equal NOFOLLOW_RESULT, doc.xpath('/html/body').inner_html
+          assert_equal doc, result
+        end
+      end
+    end
+
+    context "#scrub_document" do
+      should "be a shortcut for parse-and-scrub" do
+        mock_doc = mock
+        Loofah.expects(:document).with(:string_or_io).returns(mock_doc)
+        mock_doc.expects(:scrub!).with(:method)
+
+        Loofah.scrub_document(:string_or_io, :method)
+      end
+    end
+
+    context "#to_s" do
+      should "generate HTML" do
+        doc = Loofah.scrub_document "<html><head><title>quux</title></head><body><div>foo</div></body></html>", :prune
+        assert_not_nil doc.xpath("/html").first
+        assert_not_nil doc.xpath("/html/head").first
+        assert_not_nil doc.xpath("/html/body").first
+
+        string = doc.to_s
+        assert_contains string, /<!DOCTYPE/
+        assert_contains string, /<html>/
+        assert_contains string, /<head>/
+        assert_contains string, /<body>/
+      end
+    end
+
+    context "#serialize" do
+      should "generate HTML" do
+        doc = Loofah.scrub_document "<html><head><title>quux</title></head><body><div>foo</div></body></html>", :prune
+        assert_not_nil doc.xpath("/html").first
+        assert_not_nil doc.xpath("/html/head").first
+        assert_not_nil doc.xpath("/html/body").first
+
+        string = doc.serialize
+        assert_contains string, /<!DOCTYPE/
+        assert_contains string, /<html>/
+        assert_contains string, /<head>/
+        assert_contains string, /<body>/
+      end
+    end
+
+    context "Node" do
+      context "#scrub!" do
+        should "only scrub subtree" do
+          xml = Loofah.document <<-EOHTML
+           <html><body>
+             <div class='scrub'>
+               <script>I should be removed</script>
+             </div>
+             <div class='noscrub'>
+               <script>I should remain</script>
+             </div>
+           </body></html>
+          EOHTML
+          node = xml.at_css "div.scrub"
+          node.scrub!(:prune)
+          assert_contains         xml.to_s, /I should remain/
+          assert_does_not_contain xml.to_s, /I should be removed/
+        end
+      end
+    end
+
+    context "NodeSet" do
+      context "#scrub!" do
+        should "only scrub subtrees" do
+          xml = Loofah.document <<-EOHTML
+            <html><body>
+              <div class='scrub'>
+                <script>I should be removed</script>
+              </div>
+              <div class='noscrub'>
+                <script>I should remain</script>
+              </div>
+              <div class='scrub'>
+                <script>I should also be removed</script>
+              </div>
+            </body></html>
+          EOHTML
+          node_set = xml.css "div.scrub"
+          assert_equal 2, node_set.length
+          node_set.scrub!(:prune)
+          assert_contains         xml.to_s, /I should remain/
+          assert_does_not_contain xml.to_s, /I should be removed/
+          assert_does_not_contain xml.to_s, /I should also be removed/
+        end
+      end
+    end
   end
+  
+  context "DocumentFragment" do
+    context "#scrub!" do
+      context ":escape" do
+        should "escape bad tags" do
+          doc = Loofah::HTML::DocumentFragment.parse "<div>#{INVALID_FRAGMENT}</div>"
+          result = doc.scrub! :escape
 
-  def test_fragment_escape_bad_tags
-    doc = Loofah::HTML::DocumentFragment.parse "<div>#{INVALID_FRAGMENT}</div>"
-    result = doc.scrub! :escape
+          assert_equal INVALID_ESCAPED, doc.xpath("./div").inner_html
+          assert_equal doc, result
+        end
+      end
 
-    assert_equal INVALID_ESCAPED, doc.xpath("./div").inner_html
-    assert_equal doc, result
+      context ":prune" do
+        should "prune bad tags" do
+          doc = Loofah::HTML::DocumentFragment.parse "<div>#{INVALID_FRAGMENT}</div>"
+          result = doc.scrub! :prune
+
+          assert_equal INVALID_PRUNED, doc.xpath("./div").inner_html
+          assert_equal doc, result
+        end
+      end
+
+      context ":strip" do
+        should "strip bad tags" do
+          doc = Loofah::HTML::DocumentFragment.parse "<div>#{INVALID_FRAGMENT}</div>"
+          result = doc.scrub! :strip
+
+          assert_equal INVALID_STRIPPED, doc.xpath("./div").inner_html
+          assert_equal doc, result
+        end
+      end
+
+      context ":whitewash" do
+        should "whitewash the markup" do
+          doc = Loofah::HTML::DocumentFragment.parse "<div>#{WHITEWASH_FRAGMENT}</div>"
+          result = doc.scrub! :whitewash
+
+          assert_equal WHITEWASH_RESULT, doc.xpath("./div").inner_html
+          assert_equal doc, result
+        end
+      end
+
+      context ":nofollow" do
+        should "add a 'nofollow' attribute to hyperlinks" do
+          doc = Loofah::HTML::DocumentFragment.parse "<div>#{NOFOLLOW_FRAGMENT}</div>"
+          result = doc.scrub! :nofollow
+
+          assert_equal NOFOLLOW_RESULT, doc.xpath("./div").inner_html
+          assert_equal doc, result
+        end
+      end
+    end
+
+    context "#scrub_fragment" do
+      should "be a shortcut for parse-and-scrub" do
+        mock_doc = mock
+        Loofah.expects(:fragment).with(:string_or_io).returns(mock_doc)
+        mock_doc.expects(:scrub!).with(:method)
+
+        Loofah.scrub_fragment(:string_or_io, :method)
+      end
+    end
+
+    context "Node" do
+      context "#scrub!" do
+        should "only scrub subtree" do
+          xml = Loofah.fragment <<-EOHTML
+            <div class='scrub'>
+              <script>I should be removed</script>
+            </div>
+            <div class='noscrub'>
+              <script>I should remain</script>
+            </div>
+          EOHTML
+          node = xml.at_css "div.scrub"
+          node.scrub!(:prune)
+          assert_contains         xml.to_s, /I should remain/
+          assert_does_not_contain xml.to_s, /I should be removed/
+        end
+      end
+    end
+
+    context "NodeSet" do
+      context "#scrub!" do
+        should "only scrub subtrees" do
+          xml = Loofah.fragment <<-EOHTML
+            <div class='scrub'>
+              <script>I should be removed</script>
+            </div>
+            <div class='noscrub'>
+              <script>I should remain</script>
+            </div>
+            <div class='scrub'>
+              <script>I should also be removed</script>
+            </div>
+          EOHTML
+          node_set = xml.css "div.scrub"
+          assert_equal 2, node_set.length
+          node_set.scrub!(:prune)
+          assert_contains         xml.to_s, /I should remain/
+          assert_does_not_contain xml.to_s, /I should be removed/
+          assert_does_not_contain xml.to_s, /I should also be removed/
+        end
+      end
+    end
   end
-
-  def test_document_prune_bad_tags
-    doc = Loofah::HTML::Document.parse "<html><body>#{INVALID_FRAGMENT}</body></html>"
-    result = doc.scrub! :prune
-
-    assert_equal INVALID_PRUNED, doc.xpath('/html/body').inner_html
-    assert_equal doc, result
-  end
-
-  def test_fragment_prune_bad_tags
-    doc = Loofah::HTML::DocumentFragment.parse "<div>#{INVALID_FRAGMENT}</div>"
-    result = doc.scrub! :prune
-
-    assert_equal INVALID_PRUNED, doc.xpath("./div").inner_html
-    assert_equal doc, result
-  end
-
-  def test_document_strip_bad_tags
-    doc = Loofah::HTML::Document.parse "<html><body>#{INVALID_FRAGMENT}</body></html>"
-    result = doc.scrub! :strip
-
-    assert_equal INVALID_STRIPPED, doc.xpath('/html/body').inner_html
-    assert_equal doc, result
-  end
-
-  def test_fragment_strip_bad_tags
-    doc = Loofah::HTML::DocumentFragment.parse "<div>#{INVALID_FRAGMENT}</div>"
-    result = doc.scrub! :strip
-
-    assert_equal INVALID_STRIPPED, doc.xpath("./div").inner_html
-    assert_equal doc, result
-  end
-
-  def test_document_whitewash
-    doc = Loofah::HTML::Document.parse "<html><body>#{WHITEWASH_FRAGMENT}</body></html>"
-    result = doc.scrub! :whitewash
-
-    assert_equal WHITEWASH_RESULT, doc.xpath('/html/body').inner_html
-    assert_equal doc, result
-  end
-
-  def test_fragment_whitewash
-    doc = Loofah::HTML::DocumentFragment.parse "<div>#{WHITEWASH_FRAGMENT}</div>"
-    result = doc.scrub! :whitewash
-
-    assert_equal WHITEWASH_RESULT, doc.xpath("./div").inner_html
-    assert_equal doc, result
-  end
-
-  def test_document_nofollow
-    doc = Loofah::HTML::Document.parse "<html><body>#{NOFOLLOW_FRAGMENT}</body></html>"
-    result = doc.scrub! :nofollow
-
-    assert_equal NOFOLLOW_RESULT, doc.xpath('/html/body').inner_html
-    assert_equal doc, result
-  end
-
-  def test_fragment_nofollow
-    doc = Loofah::HTML::DocumentFragment.parse "<div>#{NOFOLLOW_FRAGMENT}</div>"
-    result = doc.scrub! :nofollow
-
-    assert_equal NOFOLLOW_RESULT, doc.xpath("./div").inner_html
-    assert_equal doc, result
-  end
-
-  def test_fragment_shortcut
-    mock_doc = mock
-    Loofah.expects(:fragment).with(:string_or_io).returns(mock_doc)
-    mock_doc.expects(:scrub!).with(:method)
-
-    Loofah.scrub_fragment(:string_or_io, :method)
-  end
-
-  def test_document_shortcut
-    mock_doc = mock
-    Loofah.expects(:document).with(:string_or_io).returns(mock_doc)
-    mock_doc.expects(:scrub!).with(:method)
-
-    Loofah.scrub_document(:string_or_io, :method)
-  end
-
-  def test_document_to_s
-    doc = Loofah.scrub_document "<html><head><title>quux</title></head><body><div>foo</div></body></html>", :prune
-    assert_not_nil doc.xpath("/html").first
-    assert_not_nil doc.xpath("/html/head").first
-    assert_not_nil doc.xpath("/html/body").first
-
-    assert_contains doc.to_s, /<!DOCTYPE/
-    assert_contains doc.to_s, /<html>/
-    assert_contains doc.to_s, /<head>/
-    assert_contains doc.to_s, /<body>/
-  end
-
-  def test_document_serialize
-    doc = Loofah.scrub_document "<html><head><title>quux</title></head><body><div>foo</div></body></html>", :prune
-
-    assert_not_nil doc.xpath("/html").first
-    assert_not_nil doc.xpath("/html/head").first
-    assert_not_nil doc.xpath("/html/body").first
-
-    assert_contains doc.serialize, /<!DOCTYPE/
-    assert_contains doc.serialize, /<html>/
-    assert_contains doc.serialize, /<head>/
-    assert_contains doc.serialize, /<body>/
-  end
-
 end
