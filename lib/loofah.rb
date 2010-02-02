cfdad6b5641ea32782ad1c044fc5a18a6255b05c
Mike Dalessio <mike@csa.net>
2010-02-02 09:54:20 -0500

    version bump to 0.4.6

--- lib/loofah.rb
+++ lib/loofah.rb
@@ -26,7 +26,7 @@ require 'loofah/helpers'
 #
 module Loofah
   # The version of Loofah you are using
-  VERSION = '0.4.5'
+  VERSION = '0.4.6'
 
   # The minimum required version of Nokogiri
   REQUIRED_NOKOGIRI_VERSION = '1.3.3'
