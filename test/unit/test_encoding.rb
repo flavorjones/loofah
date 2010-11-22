c3514cbb5003e8a276fa8c5e1068b69bd3874626
Mike Dalessio <mike@csa.net>
2010-11-22 12:28:01 -0500

    encoding test should not blow up on 1.8

--- test/unit/test_encoding.rb
+++ test/unit/test_encoding.rb
@@ -3,8 +3,10 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'helper'))
 
 class TestEncoding < Test::Unit::TestCase
   def test_string_sets_encoding
-    utf8_string = "日本語"
-    escaped = Loofah.scrub_fragment(utf8_string, :escape).to_s
-    assert_equal utf8_string.encoding, escaped.encoding
+    if String.new.respond_to?(:encoding)
+      utf8_string = "日本語"
+      escaped = Loofah.scrub_fragment(utf8_string, :escape).to_s
+      assert_equal utf8_string.encoding, escaped.encoding
+    end
   end
 end
