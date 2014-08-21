1e1637cafd32e98957c04550bfb8662b67e3fd6d
Mike Dalessio <mike@csa.net>
2014-08-21 17:25:33 -0400

    Version bump to 2.0.1 and related CHANGELOG entries.

--- lib/loofah.rb
+++ lib/loofah.rb
@@ -27,7 +27,7 @@ require 'loofah/html/document_fragment'
 #
 module Loofah
   # The version of Loofah you are using
-  VERSION = '2.0.0'
+  VERSION = '2.0.1'
 
   class << self
     # Shortcut for Loofah::HTML::Document.parse
