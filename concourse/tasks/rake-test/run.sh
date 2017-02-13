7a88d8137db40a37d4c05c9033c7cbbc6c17c518
Mike Dalessio <mike.dalessio@gmail.com>
2017-02-12 23:10:10 -0500

    concourse: refactor
    * introduce a ci resource
    * use ci resource's `run.sh` script

---
+++ concourse/tasks/rake-test/run.sh
@@ -0,0 +1,28 @@
+#! /usr/bin/env bash
+
+set -e -x -u
+
+VERSION_INFO=$(ruby -v)
+RUBY_ENGINE=$(cut -d" " -f1 <<< "${VERSION_INFO}")
+RUBY_VERSION=$(cut -d" " -f2 <<< "${VERSION_INFO}")
+
+APT_UPDATED=false
+
+function rbx-engine {
+  if [[ $RUBY_ENGINE == "rubinius" ]] ; then
+    return 0
+  fi
+  return 1
+}
+
+pushd loofah
+
+  if rbx-engine ; then
+    apt-get update
+    apt-get install -y ca-certificates gcc pkg-config libxml2-dev libxslt-dev
+  fi
+
+  bundle install
+  bundle exec rake test
+
+popd
