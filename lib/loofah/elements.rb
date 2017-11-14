e8172b4afc835f49b29175ae4568a995215d9e57
MothOnMars <mothonmars@gmail.com>
2017-11-14 15:40:33 -0800

    add html5 block-level elements

--- lib/loofah/elements.rb
+++ lib/loofah/elements.rb
@@ -2,10 +2,11 @@ require 'set'
 
 module Loofah
   module Elements
-    # Block elements in HTML4
-    STRICT_BLOCK_LEVEL = Set.new %w[address blockquote center dir div dl
-      fieldset form h1 h2 h3 h4 h5 h6 hr isindex menu noframes
-      noscript ol p pre table ul]
+    # Block elements in HTML4 & 5
+    STRICT_BLOCK_LEVEL = Set.new %w[address article aside blockquote canvas center
+      details dir div dl fieldset figcaption figure form footer header
+      h1 h2 h3 h4 h5 h6 hr isindex main menu nav noframes noscript ol p pre
+      section summary table ul]
 
     # The following elements may also be considered block-level elements since they may contain block-level elements
     LOOSE_BLOCK_LEVEL = Set.new %w[dd dt frameset li tbody td tfoot th thead tr]
