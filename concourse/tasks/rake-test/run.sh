e808fb67a3b4778dd58030bdfffccfac6019fa47
Mike Dalessio <mike.dalessio@gmail.com>
2019-11-25 13:07:51 -0500

    ci: don't turn on frozen strings until after bundle install

--- concourse/tasks/rake-test/run.sh
+++ concourse/tasks/rake-test/run.sh
@@ -4,10 +4,11 @@ set -e -x -u
 
 pushd loofah
 
+  bundle install
+
   # TODO: remove this once fefc629 (2019-11-25) is far enough in the past
   export RUBYOPT="--enable-frozen-string-literal --debug=frozen-string-literal"
 
-  bundle install
   bundle exec rake test
 
 popd
