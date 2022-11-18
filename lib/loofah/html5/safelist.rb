415677f3cf7f9254f42f811e784985cd63c7407f
Mike Dalessio <mike.dalessio@gmail.com>
2022-11-18 01:11:43 -0500

    fix: do not allow "image/svg+xml" in data URIs

--- lib/loofah/html5/safelist.rb
+++ lib/loofah/html5/safelist.rb
@@ -999,7 +999,6 @@ module Loofah
                                                  "image/gif",
                                                  "image/jpeg",
                                                  "image/png",
-                                                 "image/svg+xml",
                                                  "text/css",
                                                  "text/plain",
                                                ])
