d3678b81f36a7c22e692854b2ec8897e1564dbc9
Mike Dalessio <mike.dalessio@gmail.com>
2023-04-01 13:56:19 -0400

    test(refactor): more cleanup of test_api.rb

--- test/unit/test_api.rb
+++ test/unit/test_api.rb
@@ -5,53 +5,62 @@ class UnitTestApi < Loofah::TestCase
   let(:xml_fragment) { "<div>a</div>\n<div>b</div>" }
   let(:xml) { "<root>#{xml_fragment}</root>" }
 
-  describe Loofah::HTML do
-    it "is aliased to Loofah::HTML4" do
-      assert_equal(Loofah::HTML4, Loofah::HTML)
-      assert_equal(Loofah::HTML4::Document, Loofah::HTML::Document)
-      assert_equal(Loofah::HTML4::DocumentFragment, Loofah::HTML::DocumentFragment)
+  describe Loofah do
+    it "creates html4 documents" do
+      doc = Loofah.document(html)
+      assert_kind_of(Loofah::HTML4::Document, doc)
+      assert_equal html, doc.xpath("/html/body").inner_html
     end
 
-    it "has an HTML4 name" do
-      assert_equal("Loofah::HTML4", Loofah::HTML.to_s)
-      assert_equal("Loofah::HTML4::Document", Loofah::HTML::Document.to_s)
-      assert_equal("Loofah::HTML4::DocumentFragment", Loofah::HTML::DocumentFragment.to_s)
+    it "creates html4 fragments" do
+      doc = Loofah.fragment(html)
+      assert_kind_of(Loofah::HTML4::DocumentFragment, doc)
+      assert_equal html, doc.inner_html
+    end
+
+    it "creates xml documents" do
+      doc = Loofah.xml_document(xml)
+      assert_kind_of(Loofah::XML::Document, doc)
+      assert_equal xml, doc.root.to_xml
+    end
+
+    it "creates xml fragments" do
+      doc = Loofah.xml_fragment(xml_fragment)
+      assert_kind_of(Loofah::XML::DocumentFragment, doc)
+      assert_equal xml_fragment, doc.children.to_xml
     end
   end
 
   describe Loofah::HTML4 do
-    it "creates documents" do
-      doc = Loofah.document(html)
-      assert_html_documentish doc
-    end
-
-    it "creates fragments" do
-      doc = Loofah.fragment(html)
-      assert_html_fragmentish doc
+    it "subclasses Nokogiri::HTML4" do
+      assert_includes(Loofah::HTML4::Document.ancestors, Nokogiri::HTML4::Document)
+      assert_includes(Loofah::HTML4::DocumentFragment.ancestors, Nokogiri::HTML4::DocumentFragment)
     end
 
     it "parses documents" do
       doc = Loofah::HTML4::Document.parse(html)
-      assert_html_documentish doc
+      assert_kind_of(Loofah::HTML4::Document, doc)
+      assert_equal html, doc.xpath("/html/body").inner_html
     end
 
     it "parses document fragment" do
       doc = Loofah::HTML4::DocumentFragment.parse(html)
-      assert_html_fragmentish doc
+      assert_kind_of(Loofah::HTML4::DocumentFragment, doc)
+      assert_equal html, doc.inner_html
     end
 
     it "scrubs documents" do
-      doc = Loofah.document(html).scrub!(:strip)
-      assert_html_documentish doc
+      doc = Loofah::HTML4::Document.parse(html).scrub!(:strip)
+      assert_equal html, doc.xpath("/html/body").inner_html
     end
 
     it "scrubs fragments" do
-      doc = Loofah.fragment(html).scrub!(:strip)
-      assert_html_fragmentish doc
+      doc = Loofah::HTML4::DocumentFragment.parse(html).scrub!(:strip)
+      assert_equal html, doc.inner_html
     end
 
     it "scrubs document nodes" do
-      doc = Loofah.document(html)
+      doc = Loofah::HTML4::Document.parse(html)
       assert(node = doc.at_css("div"))
       node.scrub!(:strip)
     end
@@ -88,36 +97,33 @@ class UnitTestApi < Loofah::TestCase
   end
 
   describe Loofah::XML do
-    it "creates documents" do
-      doc = Loofah.xml_document(xml)
-      assert_xml_documentish doc
-    end
-
-    it "creates fragments" do
-      doc = Loofah.xml_fragment(xml_fragment)
-      assert_xml_fragmentish doc
+    it "subclasses Nokogiri::XML" do
+      assert_includes(Loofah::XML::Document.ancestors, Nokogiri::XML::Document)
+      assert_includes(Loofah::XML::DocumentFragment.ancestors, Nokogiri::XML::DocumentFragment)
     end
 
     it "parses documents" do
       doc = Loofah::XML::Document.parse(xml)
-      assert_xml_documentish doc
+      assert_kind_of(Loofah::XML::Document, doc)
+      assert_equal xml, doc.root.to_xml
     end
 
     it "parses document fragments" do
       doc = Loofah::XML::DocumentFragment.parse(xml_fragment)
-      assert_xml_fragmentish doc
+      assert_kind_of(Loofah::XML::DocumentFragment, doc)
+      assert_equal xml_fragment, doc.children.to_xml
     end
 
     it "scrubs documents" do
       scrubber = Loofah::Scrubber.new { |node| }
       doc = Loofah.xml_document(xml).scrub!(scrubber)
-      assert_xml_documentish doc
+      assert_equal xml, doc.root.to_xml
     end
 
     it "scrubs fragments" do
       scrubber = Loofah::Scrubber.new { |node| }
       doc = Loofah.xml_fragment(xml_fragment).scrub!(scrubber)
-      assert_xml_fragmentish doc
+      assert_equal xml_fragment, doc.children.to_xml
     end
 
     it "scrubs document nodes" do
@@ -140,29 +146,17 @@ class UnitTestApi < Loofah::TestCase
     end
   end
 
-  private
+  describe Loofah::HTML do
+    it "is aliased to Loofah::HTML4" do
+      assert_equal(Loofah::HTML4, Loofah::HTML)
+      assert_equal(Loofah::HTML4::Document, Loofah::HTML::Document)
+      assert_equal(Loofah::HTML4::DocumentFragment, Loofah::HTML::DocumentFragment)
+    end
 
-  def assert_html_documentish(doc)
-    assert_kind_of Nokogiri::HTML4::Document, doc
-    assert_kind_of Loofah::HTML4::Document, doc
-    assert_equal html, doc.xpath("/html/body").inner_html
-  end
-
-  def assert_html_fragmentish(doc)
-    assert_kind_of Nokogiri::HTML4::DocumentFragment, doc
-    assert_kind_of Loofah::HTML4::DocumentFragment, doc
-    assert_equal html, doc.inner_html
-  end
-
-  def assert_xml_documentish(doc)
-    assert_kind_of Nokogiri::XML::Document, doc
-    assert_kind_of Loofah::XML::Document, doc
-    assert_equal xml, doc.root.to_xml
-  end
-
-  def assert_xml_fragmentish(doc)
-    assert_kind_of Nokogiri::XML::DocumentFragment, doc
-    assert_kind_of Loofah::XML::DocumentFragment, doc
-    assert_equal xml_fragment, doc.children.to_xml
+    it "has an HTML4 name" do
+      assert_equal("Loofah::HTML4", Loofah::HTML.to_s)
+      assert_equal("Loofah::HTML4::Document", Loofah::HTML::Document.to_s)
+      assert_equal("Loofah::HTML4::DocumentFragment", Loofah::HTML::DocumentFragment.to_s)
+    end
   end
 end
